cd317a4f19dd971d0008192db5c084522224abd6
Kirill-found <kirillpogorelyy20@gmail.com>
2025-10-14 17:24:41 +0300

    Add @/lib alias to vite.config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -13,6 +13,7 @@ export default defineConfig({
       '@/services': path.resolve(__dirname, './src/services'),
       '@/hooks': path.resolve(__dirname, './src/hooks'),
       '@/types': path.resolve(__dirname, './src/types'),
+      '@/lib': path.resolve(__dirname, './src/lib'),
       '@/utils': path.resolve(__dirname, './src/utils'),
       '@/store': path.resolve(__dirname, './src/store')
     }
